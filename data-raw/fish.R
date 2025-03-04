e6df445aa5ae0aeb5f59155ba0489ff3a01316bb
Andre Britton <andrebritton@hotmail.com>
2025-03-04 13:37:38 +0000

    Fish with converted coordinates

--- data-raw/fish.R
+++ data-raw/fish.R
@@ -8,8 +8,9 @@
 #' showing the number and types of freshwater fish.
 #' Filtered to the top 5 most popular fish
 #' for ease of use.
-#' @import here dplyr
+#' @import here dplyr sf
 
+# Load original data set ----
 fish_counts_all <- utils::read.csv(
   here::here(
     "inst",
@@ -18,11 +19,13 @@ fish_counts_all <- utils::read.csv(
   )
 )
 
+# Filter the data set to the Wessex (geographic) area ----
 fish_counts_wessex <- fish_counts_all |>
   dplyr::filter(
     AREA == "Wessex"
   )
 
+# Create a list of unique fish and return just the 5 most common ----
 fish_list <- fish_counts_wessex |>
   dplyr::group_by(
     SPECIES_NAME
@@ -39,9 +42,43 @@ fish_list <- fish_counts_wessex |>
   ) |>
   base::as.list()
 
-fish <- fish_counts_wessex |>
+# Filter the data by the list of common fish ----
+fish_shortlist <- fish_counts_wessex |>
   dplyr::filter(
     SPECIES_NAME %in% fish_list[[1]]
   )
 
+# Identify the coordinate fields from the dataset ----
+coord_fields <- c("SURVEY_RANKED_EASTING", "SURVEY_RANKED_NORTHING")
+
+# Build a data.frame of the original Easting/Northing converted to Lat/Lng ----
+fish_latlng <- fish_shortlist |>
+  sf::st_as_sf(
+    coords = coord_fields,
+    crs = 27700
+  ) |>
+  sf::st_transform(4326) |>
+  sf::st_coordinates() |>
+  base::as.data.frame()
+
+# Join the Lat/Lng to the original data and remove Easting/Northing ----
+fish <- base::cbind(
+  fish_shortlist,
+  fish_latlng
+) |>
+  dplyr::select(
+    -dplyr::all_of(coord_fields)
+  )
+
+# Add relative paths to fish images ----
+fish <- fish |>
+  dplyr::mutate(
+    image = base::file.path(
+      "inst",
+      "images",
+      base::paste0(.data$SPECIES_NAME, ".PNG")
+    )
+  )
+
+# Save the dataset ----
 usethis::use_data(fish, overwrite = TRUE)
