c89006302c6391fe4b001a732d1327f9f4720e30
Andre Britton <andrebritton@hotmail.com>
2025-03-04 14:22:06 +0000

    Navbar

--- data-raw/SOs_geo_data.R
+++ data-raw/SOs_geo_data.R
@@ -1,14 +1,18 @@
 library(dplyr)
 load("data/historic_event.rda")
 
-SOs = historic_event %>%
-  select(ï..SiteId,
-          SiteName,
-          OutfallLatitude,
-          OutfallLongitude) %>%
-  rename("SiteID" = ï..SiteId,
-         "latitude" = OutfallLatitude,
-         "longitude" = OutfallLongitude) %>%
+SOs <- historic_event %>%
+  select(
+    SiteId,
+    SiteName,
+    OutfallLatitude,
+    OutfallLongitude
+  ) %>%
+  rename(
+    "SiteID" = SiteId,
+    "latitude" = OutfallLatitude,
+    "longitude" = OutfallLongitude
+  ) %>%
   distinct() %>%
   tidyr::drop_na() %>%
   mutate(popup = glue::glue("<b> Site Name :</b> {SiteName} <br>
