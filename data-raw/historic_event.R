0716899e3cad1c422ed240c59ddc0533e09535a9
Andre Britton <andrebritton@hotmail.com>
2025-03-04 10:57:55 +0000

    Added data-raw

---
+++ data-raw/historic_event.R
@@ -0,0 +1,29 @@
+# dependencies ----
+library(here)
+
+# load "old" historic events ----
+historic_event_old <- utils::read.csv(
+  file = here::here(
+    "data-raw",
+    "Wessex_Water_historical_Event_Duration_Monitoring_2020_2023.csv"
+  )
+)
+
+# load "new" historic events ----
+historic_event_new <- utils::read.csv(
+  file = here::here(
+    "data-raw",
+    "Wessex_Water_historical_Event_Duration_Monitoring_2024.csv"
+  )
+)
+
+# ensure field names are the same ----
+names(historic_event_new) <- names(historic_event_old)
+
+# row bind the datasets together ----
+historic_event <- base::rbind(
+  historic_event_old,
+  historic_event_new
+)
+
+usethis::use_data(historic_event, overwrite = TRUE)
